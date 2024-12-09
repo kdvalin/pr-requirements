6034a8a23c36a44381919e831fcc01aa3423d287
Keith Valin <kvalin@redhat.com>
2024-12-09 15:04:22 -0500

    initial pokings, will need to setup some dummy prs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@
  * The entrypoint for the action.
  */
 import { run } from './main'
+import * as core from '@actions/core'
+
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+run().catch((error) => {
+	if (error instanceof Error) core.setFailed(error.message)
+});
